4fffc123c745a83cb6e0261994ae18da00fa0834
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-07-09 12:16:53 +0200

    Add Home Component
    Add Hero Component
    Add shared Image for Home Page

---
+++ client/src/components/Home/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <section id="hero">
+      <div className="container">
+        <div id="hero-wrapper">
+          <h1 id="hero-heading">BRINGING YOU THE BEST AUDIO GEAR</h1>
+          <p id="hero-desc">
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </p>
+          <button className="btn-1">See All Products</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Hero from "./Hero/Hero"
+import "../../styles/home.css"
+
+const Home = () => {
+  return (
+    <main>
+        <Hero />
+    </main>
+  )
+}
+
+export default Home
