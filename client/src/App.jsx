9dc7627e11ca393ab82ac71cea0d5ab2815136eb
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-07-09 11:45:42 +0200

    Add Layout Folder
    Add Header and Footer Components in to Layout folder for better structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+import Header from './components/Layouts/Header/Header';
+import Footer from './components/Layouts/Footer/Footer';
 import './styles/base.css';
 
 function App() {
