7813faa859c574782b4b37324e5db4c353e8544c
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-07-14 11:38:55 +0200

    Add Auth Service

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CatalogPage from "./pages/CatalogPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
@@ -6,16 +6,23 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import CreatePage from "./pages/CreatePage";
 import "./styles/base.css";
+import { useState } from "react";
 import AuthContext from "./context/authContext";
+import * as authService from "./services/authService";
 
 function App() {
-  
+  const [auth, setAuth] = useState({});
+  const navigate = useNavigate();
 
-  const loginSubmitHandler = (values) => {
-    console.log(values);
+  const loginSubmitHandler = async (values) => {
+    const result = await authService.login(values.email, values.password);
+
+    setAuth(result);
+    console.log(auth);
+    navigate("/");
   };
   return (
-    <AuthContext.Provider value={{loginSubmitHandler}}>
+    <AuthContext.Provider value={{ loginSubmitHandler }}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/catalog" element={<CatalogPage />} />
