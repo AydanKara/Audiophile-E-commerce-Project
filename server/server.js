f09f80386df356ec39be84ce953eff7dbecfce2c
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-07-22 19:41:35 +0300

    Seed data into server.js
    Adding 3 products by default

--- server/server.js
+++ server/server.js
@@ -1496,253 +1496,41 @@
         _id: "0a272c58-b7ea-4e09-a000-7ec988248f66",
       },
     },
-    records: {
-      i01: {
-        name: "John1",
-        val: 1,
-        _createdOn: 1613551388703,
-      },
-      i02: {
-        name: "John2",
-        val: 1,
-        _createdOn: 1613551388713,
-      },
-      i03: {
-        name: "John3",
-        val: 2,
-        _createdOn: 1613551388723,
-      },
-      i04: {
-        name: "John4",
-        val: 2,
-        _createdOn: 1613551388733,
-      },
-      i05: {
-        name: "John5",
-        val: 2,
-        _createdOn: 1613551388743,
-      },
-      i06: {
-        name: "John6",
-        val: 3,
-        _createdOn: 1613551388753,
-      },
-      i07: {
-        name: "John7",
-        val: 3,
-        _createdOn: 1613551388763,
-      },
-      i08: {
-        name: "John8",
-        val: 2,
-        _createdOn: 1613551388773,
-      },
-      i09: {
-        name: "John9",
-        val: 3,
-        _createdOn: 1613551388783,
-      },
-      i10: {
-        name: "John10",
-        val: 1,
-        _createdOn: 1613551388793,
-      },
-    },
-    catches: {
-      "07f260f4-466c-4607-9a33-f7273b24f1b4": {
+    products: {
+      "0a31020f-f30a-47f2-8690-4d6bd2e5f182": {
         _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
-        angler: "Paulo Admorim",
-        weight: 636,
-        species: "Atlantic Blue Marlin",
-        location: "Vitoria, Brazil",
-        bait: "trolled pink",
-        captureTime: 80,
-        _createdOn: 1614760714812,
-        _id: "07f260f4-466c-4607-9a33-f7273b24f1b4",
+        category: "Headphones",
+        name: "XX99 MARK II HEADPHONES",
+        image: "https://audiophile-e-commerce.onrender.com/product-xx99-mark-two-headphones/desktop/image-product.jpg",
+        price: "200",
+        description: "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
+        features: "Featuring a genuine leather head strap and premium earcups, these headphones deliver superior comfort for those who like to enjoy endless listening. It includes intuitive controls designed for any situation. Whether you’re taking a business call or just in your own personal space, the auto on/off and pause features ensure that you’ll never miss a beat. The advanced Active Noise Cancellation with built-in equalizer allow you to experience your audio world on your terms. It lets you enjoy your audio in peace, but quickly interact with your surroundings when you need to. Combined with Bluetooth 5. 0 compliant connectivity and 17 hour battery life, the XX99 Mark II headphones gives you superior sound, cutting-edge technology, and a modern design aesthetic.",
+        _createdOn: 1721663837128,
+        _id: "0a31020f-f30a-47f2-8690-4d6bd2e5f182"
       },
-      "bdabf5e9-23be-40a1-9f14-9117b6702a9d": {
+      "bd644e5c-251e-4f5d-ab9b-6dec168ccfdc": {
         _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
-        angler: "John Does",
-        weight: 554,
-        species: "Atlantic Blue Marlin",
-        location: "Buenos Aires, Argentina",
-        bait: "trolled pink",
-        captureTime: 120,
-        _createdOn: 1614760782277,
-        _id: "bdabf5e9-23be-40a1-9f14-9117b6702a9d",
+        category: "Earphones",
+        name: "YX1 WIRELESS EARPHONES",
+        image: "https://audiophile-e-commerce.onrender.com/product-yx1-earphones/desktop/image-product.jpg",
+        price: "250",
+        description: "Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature.",
+        features: "Experience unrivalled stereo sound thanks to innovative acoustic technology. With improved ergonomics designed for full day wearing, these revolutionary earphones have been finely crafted to provide you with the perfect fit, delivering complete comfort all day long while enjoying exceptional noise isolation and truly immersive sound. The YX1 Wireless Earphones features customizable controls for volume, music, calls, and voice assistants built into both earbuds. The new 7-hour battery life can be extended up to 28 hours with the charging case, giving you uninterrupted play time. Exquisite craftsmanship with a splash resistant design now available in an all new white and grey color scheme as well as the popular classic black.",
+        _createdOn: 1721664492417,
+        _id: "bd644e5c-251e-4f5d-ab9b-6dec168ccfdc"
       },
-    },
-    furniture: {},
-    orders: {},
-    movies: {
-      "1240549d-f0e0-497e-ab99-eb8f703713d7": {
+      "2778dcc9-3386-425f-be4c-fcf3cd03736d": {
         _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
-        title: "Black Widow",
-        description:
-          "Natasha Romanoff aka Black Widow confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Comes on the screens 2020.",
-        img: "https://miro.medium.com/max/735/1*akkAa2CcbKqHsvqVusF3-w.jpeg",
-        _createdOn: 1614935055353,
-        _id: "1240549d-f0e0-497e-ab99-eb8f703713d7",
-      },
-      "143e5265-333e-4150-80e4-16b61de31aa0": {
-        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
-        title: "Wonder Woman 1984",
-        description:
-          "Diana must contend with a work colleague and businessman, whose desire for extreme wealth sends the world down a path of destruction, after an ancient artifact that grants wishes goes missing.",
-        img: "https://pbs.twimg.com/media/ETINgKwWAAAyA4r.jpg",
-        _createdOn: 1614935181470,
-        _id: "143e5265-333e-4150-80e4-16b61de31aa0",
-      },
-      "a9bae6d8-793e-46c4-a9db-deb9e3484909": {
-        _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
-        title: "Top Gun 2",
-        description:
-          "After more than thirty years of service as one of the Navy's top aviators, Pete Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancement in rank that would ground him.",
-        img: "https://i.pinimg.com/originals/f2/a4/58/f2a458048757bc6914d559c9e4dc962a.jpg",
-        _createdOn: 1614935268135,
-        _id: "a9bae6d8-793e-46c4-a9db-deb9e3484909",
-      },
-    },
-    likes: {},
-    ideas: {
-      "833e0e57-71dc-42c0-b387-0ce0caf5225e": {
-        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
-        title: "Best Pilates Workout To Do At Home",
-        description:
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima possimus eveniet ullam aspernatur corporis tempore quia nesciunt nostrum mollitia consequatur. At ducimus amet aliquid magnam nulla sed totam blanditiis ullam atque facilis corrupti quidem nisi iusto saepe, consectetur culpa possimus quos? Repellendus, dicta pariatur! Delectus, placeat debitis error dignissimos nesciunt magni possimus quo nulla, fuga corporis maxime minus nihil doloremque aliquam quia recusandae harum. Molestias dolorum recusandae commodi velit cum sapiente placeat alias rerum illum repudiandae? Suscipit tempore dolore autem, neque debitis quisquam molestias officia hic nesciunt? Obcaecati optio fugit blanditiis, explicabo odio at dicta asperiores distinctio expedita dolor est aperiam earum! Molestias sequi aliquid molestiae, voluptatum doloremque saepe dignissimos quidem quas harum quo. Eum nemo voluptatem hic corrupti officiis eaque et temporibus error totam numquam sequi nostrum assumenda eius voluptatibus quia sed vel, rerum, excepturi maxime? Pariatur, provident hic? Soluta corrupti aspernatur exercitationem vitae accusantium ut ullam dolor quod!",
-        img: "./images/best-pilates-youtube-workouts-2__medium_4x3.jpg",
-        _createdOn: 1615033373504,
-        _id: "833e0e57-71dc-42c0-b387-0ce0caf5225e",
-      },
-      "247efaa7-8a3e-48a7-813f-b5bfdad0f46c": {
-        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
-        title: "4 Eady DIY Idea To Try!",
-        description:
-          "Similique rem culpa nemo hic recusandae perspiciatis quidem, quia expedita, sapiente est itaque optio enim placeat voluptates sit, fugit dignissimos tenetur temporibus exercitationem in quis magni sunt vel. Corporis officiis ut sapiente exercitationem consectetur debitis suscipit laborum quo enim iusto, labore, quod quam libero aliquid accusantium! Voluptatum quos porro fugit soluta tempore praesentium ratione dolorum impedit sunt dolores quod labore laudantium beatae architecto perspiciatis natus cupiditate, iure quia aliquid, iusto modi esse!",
-        img: "./images/brightideacropped.jpg",
-        _createdOn: 1615033452480,
-        _id: "247efaa7-8a3e-48a7-813f-b5bfdad0f46c",
-      },
-      "b8608c22-dd57-4b24-948e-b358f536b958": {
-        _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
-        title: "Dinner Recipe",
-        description:
-          "Consectetur labore et corporis nihil, officiis tempora, hic ex commodi sit aspernatur ad minima? Voluptas nesciunt, blanditiis ex nulla incidunt facere tempora laborum ut aliquid beatae obcaecati quidem reprehenderit consequatur quis iure natus quia totam vel. Amet explicabo quidem repellat unde tempore et totam minima mollitia, adipisci vel autem, enim voluptatem quasi exercitationem dolor cum repudiandae dolores nostrum sit ullam atque dicta, tempora iusto eaque! Rerum debitis voluptate impedit corrupti quibusdam consequatur minima, earum asperiores soluta. A provident reiciendis voluptates et numquam totam eveniet! Dolorum corporis libero dicta laborum illum accusamus ullam?",
-        img: "./images/dinner.jpg",
-        _createdOn: 1615033491967,
-        _id: "b8608c22-dd57-4b24-948e-b358f536b958",
-      },
-    },
-    catalog: {
-      "53d4dbf5-7f41-47ba-b485-43eccb91cb95": {
-        _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
-        make: "Table",
-        model: "Swedish",
-        year: 2015,
-        description: "Medium table",
-        price: 235,
-        img: "./images/table.png",
-        material: "Hardwood",
-        _createdOn: 1615545143015,
-        _id: "53d4dbf5-7f41-47ba-b485-43eccb91cb95",
-      },
-      "f5929b5c-bca4-4026-8e6e-c09e73908f77": {
-        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
-        make: "Sofa",
-        model: "ES-549-M",
-        year: 2018,
-        description: "Three-person sofa, blue",
-        price: 1200,
-        img: "./images/sofa.jpg",
-        material: "Frame - steel, plastic; Upholstery - fabric",
-        _createdOn: 1615545572296,
-        _id: "f5929b5c-bca4-4026-8e6e-c09e73908f77",
-      },
-      "c7f51805-242b-45ed-ae3e-80b68605141b": {
-        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
-        make: "Chair",
-        model: "Bright Dining Collection",
-        year: 2017,
-        description: "Dining chair",
-        price: 180,
-        img: "./images/chair.jpg",
-        material: "Wood laminate; leather",
-        _createdOn: 1615546332126,
-        _id: "c7f51805-242b-45ed-ae3e-80b68605141b",
-      },
-    },
-    teams: {
-      "34a1cab1-81f1-47e5-aec3-ab6c9810efe1": {
-        _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
-        name: "Storm Troopers",
-        logoUrl: "/assets/atat.png",
-        description: "These ARE the droids we're looking for",
-        _createdOn: 1615737591748,
-        _id: "34a1cab1-81f1-47e5-aec3-ab6c9810efe1",
-      },
-      "dc888b1a-400f-47f3-9619-07607966feb8": {
-        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
-        name: "Team Rocket",
-        logoUrl: "/assets/rocket.png",
-        description: "Gotta catch 'em all!",
-        _createdOn: 1615737655083,
-        _id: "dc888b1a-400f-47f3-9619-07607966feb8",
-      },
-      "733fa9a1-26b6-490d-b299-21f120b2f53a": {
-        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
-        name: "Minions",
-        logoUrl: "/assets/hydrant.png",
-        description:
-          "Friendly neighbourhood jelly beans, helping evil-doers succeed.",
-        _createdOn: 1615737688036,
-        _id: "733fa9a1-26b6-490d-b299-21f120b2f53a",
-      },
-    },
-    members: {
-      "cc9b0a0f-655d-45d7-9857-0a61c6bb2c4d": {
-        _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
-        teamId: "34a1cab1-81f1-47e5-aec3-ab6c9810efe1",
-        status: "member",
-        _createdOn: 1616236790262,
-        _updatedOn: 1616236792930,
-      },
-      "61a19986-3b86-4347-8ca4-8c074ed87591": {
-        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
-        teamId: "dc888b1a-400f-47f3-9619-07607966feb8",
-        status: "member",
-        _createdOn: 1616237188183,
-        _updatedOn: 1616237189016,
-      },
-      "8a03aa56-7a82-4a6b-9821-91349fbc552f": {
-        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
-        teamId: "733fa9a1-26b6-490d-b299-21f120b2f53a",
-        status: "member",
-        _createdOn: 1616237193355,
-        _updatedOn: 1616237195145,
-      },
-      "9be3ac7d-2c6e-4d74-b187-04105ab7e3d6": {
-        _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
-        teamId: "dc888b1a-400f-47f3-9619-07607966feb8",
-        status: "member",
-        _createdOn: 1616237231299,
-        _updatedOn: 1616237235713,
-      },
-      "280b4a1a-d0f3-4639-aa54-6d9158365152": {
-        _ownerId: "60f0cf0b-34b0-4abd-9769-8c42f830dffc",
-        teamId: "dc888b1a-400f-47f3-9619-07607966feb8",
-        status: "member",
-        _createdOn: 1616237257265,
-        _updatedOn: 1616237278248,
-      },
-      "e797fa57-bf0a-4749-8028-72dba715e5f8": {
-        _ownerId: "60f0cf0b-34b0-4abd-9769-8c42f830dffc",
-        teamId: "34a1cab1-81f1-47e5-aec3-ab6c9810efe1",
-        status: "member",
-        _createdOn: 1616237272948,
-        _updatedOn: 1616237293676,
-      },
-    },
-    
+        category: "Speakers",
+        name: "ZX9 SPEAKER",
+        image: "https://audiophile-e-commerce.onrender.com/product-zx9-speaker/desktop/image-product.jpg",
+        price: "300",
+        description: "Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
+        features: "Connect via Bluetooth or nearly any wired source. This speaker features optical, digital coaxial, USB Type-B, stereo RCA, and stereo XLR inputs, allowing you to have up to five wired source devices connected for easy switching. Improved bluetooth technology offers near lossless audio quality at up to 328ft (100m). Discover clear, more natural sounding highs than the competition with ZX9’s signature planar diaphragm tweeter. Equally important is its powerful room-shaking bass courtesy of a 6.5” aluminum alloy bass unit. You’ll be able to enjoy equal sound quality whether in a large room or small den. Furthermore, you will experience new sensations from old songs since it can respond to even the subtle waveforms.",
+        _createdOn: 1721664547583,
+        _id: "2778dcc9-3386-425f-be4c-fcf3cd03736d"
+      }
+    }
   };
   var rules$1 = {
     users: {
