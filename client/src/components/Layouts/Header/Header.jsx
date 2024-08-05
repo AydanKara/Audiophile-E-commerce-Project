4450680b7d9e6d2103d3b93b896edb24e057d823
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-08-05 23:59:07 +0300

    Update Header.jsx

--- client/src/components/Layouts/Header/Header.jsx
+++ client/src/components/Layouts/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Header.css";
 import { useContext } from "react";
 import AuthContext from "../../../context/authContext";
@@ -9,52 +9,73 @@ const Header = () => {
     <header>
       <div className="container">
         <div id="header-wrapper">
-          <Link className="logo" to="/">
+          <NavLink className="logo" to="/">
             <img src="/shared/logo.svg" alt="website logo" />
-          </Link>
+          </NavLink>
           <nav>
             <ul id="nav-list">
               <li className="nav-item">
-                <NavLink to="/" activeClassName="active">
+                <NavLink
+                  to="/"
+                  className={({ isActive }) => (isActive ? "active" : "")}
+                >
                   HOME
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/catalog" activeClassName="active">
+                <NavLink
+                  to="/catalog"
+                  className={({ isActive }) => (isActive ? "active" : "")}
+                >
                   Catalog
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/contact" activeClassName="active">
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) => (isActive ? "active" : "")}
+                >
                   Contact
                 </NavLink>
               </li>
               {isAuthenticated && (
                 <>
                   <li className="nav-item">
-                    <NavLink to="/create" activeClassName="active">
+                    <NavLink
+                      to="/create"
+                      className={({ isActive }) => (isActive ? "active" : "")}
+                    >
                       Create
                     </NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink to="/profile" activeClassName="active">
+                    <NavLink
+                      to="/profile"
+                      className={({ isActive }) => (isActive ? "active" : "")}
+                    >
                       Profile
                     </NavLink>
                   </li>
                   <li className="nav-item">
-                    <Link to="/logout">Logout</Link>
+                    <NavLink to="/logout">Logout</NavLink>
                   </li>
                 </>
               )}
               {!isAuthenticated && (
                 <>
                   <li className="nav-item">
-                    <NavLink to="/register" activeClassName="active">
+                    <NavLink
+                      to="/register"
+                      className={({ isActive }) => (isActive ? "active" : "")}
+                    >
                       Register
                     </NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink to="/login" activeClassName="active">
+                    <NavLink
+                      to="/login"
+                      className={({ isActive }) => (isActive ? "active" : "")}
+                    >
                       Login
                     </NavLink>
                   </li>
