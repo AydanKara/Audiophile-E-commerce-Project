ce154ac3fd5c6d32d9f0ce1f8b367e6c4de58525
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-09-10 08:29:44 +0200

    Create User.js
    create user model

---
+++ backend/models/User.js
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    hashedPassword: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.index(
+  { email: 1 },
+  {
+    collation: {
+      locale: "en",
+      strength: 2,
+    },
+  }
+);
+
+const User = model("User", userSchema);
+
+export default User;
